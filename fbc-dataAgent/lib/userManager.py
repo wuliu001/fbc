0006797f7eb2df77526402ccd62d7355d51a2d2a
tao <67364336@qq.com>
2018-12-26 17:08:22 +0800

    All the user management function

--- fbc-dataAgent/lib/userManager.py
+++ fbc-dataAgent/lib/userManager.py
@@ -1,10 +1,14 @@
 # -*- coding: utf8 -*-
 
-#import sys
+import urllib
 import crypto_utility
+import restful_utility
+import json
 
-def register(body):
+def register(server_url, body):
     crypto_utility.unify_encoding()
     public_key, private_key = crypto_utility.get_key()
-    data = str(public_key)+'\n'+str(private_key)
-    return '200 OK', [('Content-Type','text/html')], [data + '\n'], 
+    ds_url = server_url + '/users/insert?pubkey='+urllib.quote(str(public_key))+'&prikey='+urllib.quote(str(private_key))
+    code, json_obj = restful_utility.restful_runner(ds_url, "POST", None, body)
+    data = json.dumps(json_obj)
+    return '200 OK', [('Content-Type','text/html')], [data + '\n']
