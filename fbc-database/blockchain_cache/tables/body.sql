fa409fd9048f674b42c8e8d6ebf3db789c088d6e
Qiao Li <Qiao.Li@ygomi.com>
2019-01-26 08:38:50 +0800

    master revise table structure

---
+++ fbc-database/blockchain_cache/tables/body.sql
@@ -0,0 +1,12 @@
+USE `blockchain_cache`;
+
+/*Table structure for table `body` */;
+
+DROP TABLE IF EXISTS `body`;
+
+CREATE TABLE `body` (
+  `id`        BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `header`    VARCHAR(256) NOT NULL,
+  `hash`      VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
---
+++ fbc-database/blockchain_cache/tables/header.sql
@@ -0,0 +1,17 @@
+USE `blockchain_cache`;
+
+/*Table structure for table `header` */;
+
+DROP TABLE IF EXISTS `header`;
+
+CREATE TABLE `header` (
+  `id`           BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `parentHash`   VARCHAR(256) NOT NULL,
+  `stateRoot`    VARCHAR(256) NOT NULL,
+  `txRoot`       VARCHAR(256) NOT NULL,
+  `receiptRoot`  VARCHAR(256) NOT NULL,
+  `bloom`        LONGTEXT NOT NULL,
+  `time`         DATETIME,
+  `nonce`        INT(11),
+  PRIMARY KEY    (`id`)
+) ENGINE=InnoDB;
