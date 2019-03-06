4343297ecb206710f2fe2fa196d8fdc6193442bb
Qiao Li <Qiao.Li@ygomi.com>
2019-03-06 19:56:42 +0800

    master add new tables in blockchain_cache schema

---
+++ fbc-database/blockchain_cache/tables/transaction_trie.sql
@@ -0,0 +1,15 @@
+USE `blockchain_cache`;
+
+/*Table structure for table `transaction_trie` */;
+
+DROP TABLE IF EXISTS `transaction_trie`;
+
+CREATE TABLE `transaction_trie` (
+  `id`          BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `parentHash`  VARCHAR(256) NOT NULL,
+  `hash`        VARCHAR(256) NOT NULL,
+  `alias`       VARCHAR(200) NOT NULL,
+  `layer`       INT NOT NULL,
+  `address`     VARCHAR(256) DEFAULT NULL,
+  PRIMARY KEY   (`id`)
+) ENGINE=InnoDB;
---
+++ fbc-database/blockchain_cache/tables/transactions.sql
@@ -0,0 +1,23 @@
+USE `blockchain_cache`;
+
+/*Table structure for table `transactions` */;
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `address`                   VARCHAR(256) NOT NULL,
+  `initiator`                 VARCHAR(256) NOT NULL,
+  `nonceForCurrentInitiator`  BIGINT(20) NOT NULL,
+  `nonceForOriginInitiator`   BIGINT(20) NOT NULL,
+  `nonceForSmartContract`     BIGINT(20) DEFAULT NULL,
+  `receiver`                  VARCHAR(256) NOT NULL,
+  `txType`                    VARCHAR(32) NOT NULL,
+  `detail`                    LONGTEXT NOT NULL,
+  `gasCost`                   FLOAT NOT NULL,
+  `gasDeposit`                FLOAT NOT NULL,
+  `hashSign`                  VARCHAR(256) NOT NULL,
+  `receiptAddress`            VARCHAR(256) NOT NULL,
+  `createTime`                DATETIME NOT NULL,
+  `closeTime`                 DATETIME DEFAULT NULL,
+  PRIMARY KEY                 (`address`)
+) ENGINE=InnoDB;
