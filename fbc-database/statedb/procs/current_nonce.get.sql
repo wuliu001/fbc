0fd5ccb913867a8c1b2199f2540fd7641e8cf22c
Shiliu Liu <Shiliu.Liu@ygomi.com>
2019-03-06 20:37:26 +0800

    add cache data

--- fbc-database/statedb/procs/current_nonce.get.sql
+++ fbc-database/statedb/procs/current_nonce.get.sql
@@ -12,7 +12,7 @@ CREATE PROCEDURE `current_nonce.get`(
     OUT returnCode_o         INT,
     OUT returnMsg_o          LONGTEXT)
 ll:BEGIN
-    DECLARE v_cnt            BIGINT(20);
+    DECLARE v_cnt            INT;
     DECLARE v_procname       VARCHAR(100) DEFAULT 'current_nonce.get';
     DECLARE v_modulename     VARCHAR(50) DEFAULT 'statedb';
     DECLARE v_params_body    LONGTEXT DEFAULT '';
@@ -38,10 +38,24 @@ ll:BEGIN
     SET returnMsg_o = 'check input data validation error.';
     IF IFNULL(account_addr_i,'') = '' THEN
         SET returnCode_o = 511;
-        CALL `commons`.`log_module.e`(0,v_modulename,v_procname,v_params_body,body_i,returnMsg_o,v_returnCode,v_returnMsg);
+        CALL `commons`.`log_module.e`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
         LEAVE ll;
     END IF;
     
+    SET returnMsg_o = 'check account_addr_i not exists.';
+    SELECT COUNT(1) INTO v_cnt FROM statedb.state_object  WHERE accountAddress = account_addr_i;
+    IF v_cnt = 0 THEN
+        SET returnCode_o = 512;
+        CALL `commons`.`log_module.e`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
+        LEAVE ll;
+    END IF;    
+    
+    SELECT IFNULL(MAX(GREATEST(a.nonce,IFNULL(b.nonce,0))),0) AS current_user_nonce
+      FROM statedb.state_object a
+      LEFT 
+      JOIN tx_cache.transactions b ON a.accountAddress = b.accountAddress AND a.accountAddress = account_addr_i AND b.delete_flag = 0;
+    
+    /*
     SELECT IFNULL(MAX(nonce),0) 
       INTO v_state_nonce
       FROM statedb.state_object 
@@ -52,7 +66,10 @@ ll:BEGIN
       FROM tx_cache.transactions 
      WHERE accountAddress = account_addr_i;
     
+    
     SELECT IFNULL(v_state_nonce,v_tx_trans_nonce) AS  current_user_nonce;
+    
+    */
     
     SET returnCode_o = 200;
 	SET returnMsg_o = 'OK';
--- fbc-database/tx_cache/procs/cache_detail.get.sql
+++ fbc-database/tx_cache/procs/cache_detail.get.sql
@@ -20,13 +20,14 @@ ll:BEGIN
     DECLARE v_params_body    LONGTEXT DEFAULT '';
     DECLARE v_returnCode     INT;
     DECLARE v_returnMsg      LONGTEXT;
+    DECLARE v_cur_timestamp  BIGINT(20) UNSIGNED DEFAULT FLOOR(UNIX_TIMESTAMP(CURRENT_TIMESTAMP(6))*1000000);
     
 	DECLARE EXIT HANDLER FOR SQLWARNING, SQLEXCEPTION BEGIN
         SHOW WARNINGS;
         GET DIAGNOSTICS CONDITION 1 v_returnCode = MYSQL_ERRNO, v_returnMsg = MESSAGE_TEXT;
-        
+        COMMIT;
         SET returnCode_o = 400;
-        SET returnMsg_o = CONCAT(v_modulename, '-', v_procname, ' execute Error: ', IFNULL(returnMsg_o,'') , ' | ' ,v_returnMsg);
+        SET returnMsg_o = CONCAT(v_modulename, '-', v_procname, ' command Error: ', IFNULL(returnMsg_o,'') , ' | ' ,v_returnMsg);
         CALL `commons`.`log_module.e`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
     END;
 
@@ -45,10 +46,26 @@ ll:BEGIN
     END IF;
     
     SET returnMsg_o = 'fail to check nonce continuity';
+    CREATE TEMPORARY TABLE IF NOT EXISTS tx_cache.temp_fueg_domains (
+      `id`                          BIGINT(20) NOT NULL AUTO_INCREMENT,
+      `domains`                     LONGTEXT,
+      `edge_nodes`                  LONGTEXT,
+      `keys`                        LONGTEXT,
+      PRIMARY KEY                   (`id`)
+    ) ENGINE=InnoDB;
+    TRUNCATE TABLE history.temp_fueg_domains;
     
-    SELECT  txAddress, accountAddress, transactionType, blockObject, hashSign, gasCost, gasDeposit, nonce, `timestamp`, comfirmedTimes FROM tx_cache.transactions where accountAddress=accountAddress_i;
-    SELECT  accountAddress, publicKey, creditRating, balance, smartContractPrice, minSmartContractDeposit, nonce FROM tx_cache.state_object where accountAddress=accountAddress_i;
-
+    START TRANSACTION;
+    SET SESSION innodb_lock_wait_timeout = 30;
+    
+    SELECT txAddress, accountAddress, transactionType, blockObject, hashSign, gasCost, gasDeposit, nonce, `timestamp`, comfirmedTimes FROM tx_cache.transactions where accountAddress=accountAddress_i;
+    
+    SET returnMsg_o = 'fail to return cache state_object datas';
+    SELECT accountAddress, publicKey, creditRating, balance, smartContractPrice, minSmartContractDeposit, nonce 
+      FROM tx_cache.state_object 
+     WHERE delete_flag = 0;
+    
+    COMMIT;
     SET returnCode_o = 200;
 	SET returnMsg_o = 'OK';
     CALL `commons`.`log_module.i`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
