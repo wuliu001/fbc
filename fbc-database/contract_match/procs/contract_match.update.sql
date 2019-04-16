3abfc160a2137c09f972466ba006ec2f1f7ea4fc
Shiliu Liu <Shiliu.Liu@ygomi.com>
2019-04-16 19:27:37 +0800

    add contract

---
+++ fbc-database/contract_match/procs/contract_match.update.sql
@@ -0,0 +1,41 @@
+USE `contract_match`;
+/*!50003 SET @saved_sql_mode = @@sql_mode */;
+/*!50003 SET sql_mode = 'STRICT_ALL_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */;
+
+/*Procedure structure for Procedure `contract_match.update` */;
+
+DROP PROCEDURE IF EXISTS `contract_match.update`;
+
+DELIMITER $$
+CREATE PROCEDURE `contract_match.update`(
+    OUT returnCode_o         INT,
+    OUT returnMsg_o          LONGTEXT)
+ll:BEGIN
+    DECLARE v_cnt            BIGINT(20);
+    DECLARE v_procname       VARCHAR(100) DEFAULT 'contract_match.update';
+    DECLARE v_modulename     VARCHAR(50) DEFAULT 'contract_match';
+    DECLARE v_params_body    LONGTEXT DEFAULT '';
+    DECLARE v_returnCode     INT;
+    DECLARE v_returnMsg      LONGTEXT;
+    
+	DECLARE EXIT HANDLER FOR SQLWARNING, SQLEXCEPTION BEGIN
+        SHOW WARNINGS;
+        GET DIAGNOSTICS CONDITION 1 v_returnCode = MYSQL_ERRNO, v_returnMsg = MESSAGE_TEXT;
+        
+        SET returnCode_o = 400;
+        SET returnMsg_o = CONCAT(v_modulename, '-', v_procname, ' execute Error: ', IFNULL(returnMsg_o,'') , ' | ' ,v_returnMsg);
+        CALL `commons`.`log_module.e`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
+    END;
+
+    SET v_params_body = CONCAT('{}');
+
+
+ 
+    
+    SET returnCode_o = 200;
+	SET returnMsg_o = 'OK';
+    CALL `commons`.`log_module.i`(0,v_modulename,v_procname,v_params_body,NULL,returnMsg_o,v_returnCode,v_returnMsg);
+END
+$$
+DELIMITER ;
+/*!50003 SET sql_mode = @saved_sql_mode */;
